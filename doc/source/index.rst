88a391abfaa8de9eabaa630805dfaed2d9b25790
Danilo Bargen <danilo@factor.ch>
2012-03-30 13:27:47 +0200

    Added and improved documentation using sphinx

---
+++ doc/source/index.rst
@@ -0,0 +1,23 @@
+.. django-tabination documentation master file, created by
+   sphinx-quickstart on Fri Mar 30 11:09:06 2012.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+django-tabination
+=================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   views
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- tabination/views.py
+++ tabination/views.py
@@ -1,8 +1,13 @@
+"""
+.. moduleauthor:: Danilo Bargen <gezuru@gmail.com>
+
+"""
+
 from django.core.exceptions import ImproperlyConfigured
 from django.views.generic import TemplateView
 
 
-class TabTracker(type):
+class _TabTracker(type):
     """Metaclass that tracks all subclasses that have set the _is_tab
     attribute. The classes are stored inside self._registry."""
     def __init__(cls, name, bases, attrs):
@@ -19,28 +24,29 @@ class TabView(TemplateView):
         @property
         def tab_visible(self):
            return self.object.is_active
-    
-    Attributes:
-    tab_id -- ID of the tab, can be used for URL and more.
-    tab_label -- Text on the tab. If label is None, tab will be hidden.
-    tab_group -- All tabs with the same group string will be listed.
-    tab_counter -- A string or callable with a count that will be shown
-                   on the tab.
-    tab_classes -- A list of CSS classes that will be added to the tab.
-    tab_rel -- String that will be added to the HTML rel element.
-    tab_visible -- Whether or not this tab is shown in the tab group.
 
+    Internally, the tabs are tracked using the :class:`_TabTracker`
+    `metaclass <http://stackoverflow.com/a/6581949/284318>`_.
+    All subclasses that have set the `_is_tab` attribute are added to the
+    `self._registry` list.
+    
     """
 
-    __metaclass__ = TabTracker
+    __metaclass__ = _TabTracker
     _registry = []
+    """In here, references to all tabs are stored."""
     tab_id = None
+    """ID of the tab, can be used for URL, CSS classes and more."""
     tab_label = None
+    """Label for the tab. If label is `None`, tab should be hidden."""
     tab_group = None
+    """All tabs with the same group string will be contained in `{{ tabs }}`."""
     tab_counter = None
+    """A string or callable with a count that can be shown on the tab."""
     tab_classes = []
-    tab_rel = u''
-    tab_visible = True
+    """A list of CSS classes that can be added to the tab."""
+    tab_rel = ''
+    """String that can be set as the HTML rel element."""
 
     def get_group_tabs(self):
         """Return instances of all other tabs that are members of the
@@ -54,10 +60,17 @@ class TabView(TemplateView):
 
     @property
     def tab_visible(self):
+        """Whether or not this tab is shown in the tab group. Or to be more exact,
+        whether or not this tab is contained in `{{ tabs }}`.
+        
+        The default behavior is to set the tab as visible if it has a label.
+        
+        """
         return self.tab_label is not None
     
     def get_context_data(self, **kwargs):
-        """Add tab information to context."""
+        """Add tab information to context. To retrieve list of all group tab
+        instances, use `{{ tabs }}` in your template."""
 
         # Get base context
         context = super(TabView, self).get_context_data()
@@ -65,7 +78,7 @@ class TabView(TemplateView):
         # Update the context with kwargs, as TemplateView doesn't do this
         context.update(kwargs)
 
-        # Get all group tabs
+        # Get all visible group tabs
         tabs = [t for t in self.get_group_tabs() if t.tab_visible]
 
         # We need tab instances of all tab group members in order to

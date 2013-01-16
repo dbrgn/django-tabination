b4e5214f813fe5a0aaedeef35ec0c1d308aac126
Markus Zapke-Gründemann <markus@keimlink.de>
2013-01-16 18:13:01 +0100

    Fixed indentation.

--- tabination/views.py
+++ tabination/views.py
@@ -53,8 +53,8 @@ class TabView(TemplateView):
         tab's tab group."""
         if self.tab_group is None:
             raise ImproperlyConfigured(
-                "%s requires a definition of 'tab_group'" \
-                    % self.__class__.__name__)
+                "%s requires a definition of 'tab_group'" %
+                self.__class__.__name__)
         group_members = [t for t in self._registry if t.tab_group == self.tab_group]
         return [t() for t in group_members]
 
